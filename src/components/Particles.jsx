da32776638485002da021dda431695ec2ea1e4a7
Ben-Zard <you@example.com>
2022-09-15 13:36:30 -0400

    add Portfolio

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -3,9 +3,8 @@ import { loadFull } from "tsparticles";
 import options from "../config/particles-config.json";
 export default function ParticleEffect() {
   const particlesInit = async (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        console.log(main);
+ // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(main);
@@ -22,3 +21,4 @@ export default function ParticleEffect() {
     </>
   );
 }
+
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,9 @@
+import PortfolioDisplay from '../components/PortfolioDisplay'
+
+const Portfolio = () => {
+  return (
+    <PortfolioDisplay/>
+  )
+}
+
+export default Portfolio
